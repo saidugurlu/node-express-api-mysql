ba5c7d7ab4eb16addf86b3c8789c632ac305ddd2
saidugurlu <ugurlumsaid@gmx.de>
2022-06-28 11:48:41 +0200

    mysql installed and import

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import mysql from 'mysql'
  
 const app = express();
 const port = 3044;
  
 app.get('/', (req, res) => {
-    res.send('test api');
+    res.send("<h1>Customer API</h1>");
 });
  
 app.listen(port, () => {
