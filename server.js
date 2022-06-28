65b4d4d20d332d4973219d4bd64ae33a51e7d773
saidugurlu <ugurlumsaid@gmx.de>
2022-06-28 11:53:17 +0200

    route added

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const port = 3044;
 app.get('/', (req, res) => {
     res.send("<h1>Customer API</h1>");
 });
+
+
+app.get('/customers', (req,res) => {
+ 
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'webuser',
+        password: 'passpass',
+        database: 'northwind'
+    });
+
+    connection.connect((err) => {
+        if (err) throw err;
+        const sql = 'SELECT company,last_name,first_name FROM customers';
+        connection.query(sql, (err, records) => {
+            if (err) throw err;
+            res.send(records);
+        });
+    });
+});
  
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
